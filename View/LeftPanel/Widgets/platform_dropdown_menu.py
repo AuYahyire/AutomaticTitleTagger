6b6eacebf2edbc70cdac6ccfe90042be3d1378d5
AuYahyire <augello.gian@gmail.com>
2025-01-03 00:56:16 +0100

    fixed bug with last_platform not updating.

--- View/LeftPanel/Widgets/platform_dropdown_menu.py
+++ View/LeftPanel/Widgets/platform_dropdown_menu.py
@@ -6,6 +6,7 @@ from View.LeftPanel.base_widget import BaseWidget
 
 class PlatformDropdownMenu(BaseWidget):
     def __init__(self, view_model):
+        self._updating = False  # Flag to prevent recursive updates
         super().__init__(view_model)
         self.view_model.data_manager.data_changed.connect(self.update_dropdown)
 
@@ -13,16 +14,46 @@ class PlatformDropdownMenu(BaseWidget):
         self.platform_label = QLabel("Platform:")
         self.platform_dropdown = QComboBox(self)
 
+        # Update dropdown with initial items
         self.update_dropdown()
 
-        self.view_model.data_manager.set_data('last_platform', self.platform_dropdown.currentText())
-        self.platform_dropdown.setCurrentText(self.view_model.data_manager.get_data('last_platform'))
+        # Set initial value from last_platform
+        initial_platform = self.view_model.data_manager.get_data('last_platform')
+        if initial_platform:
+            self.platform_dropdown.setCurrentText(initial_platform)
 
-        # Platform dropdown and recursive checkbox
+        # Connect the signal to handle selection changes
+        self.platform_dropdown.currentTextChanged.connect(self.on_platform_changed)
+
     def configure_layout(self, layout):
         layout.addWidget(self.platform_label)
         layout.addWidget(self.platform_dropdown)
 
     def update_dropdown(self):
-        self.platform_dropdown.clear()
-        self.platform_dropdown.addItems(self.view_model.data_manager.get_data('platforms', {}).keys())
+        if self._updating:  # Prevent recursive updates
+            return
+
+        try:
+            self._updating = True
+            current_text = self.platform_dropdown.currentText()
+            self.platform_dropdown.clear()
+            platforms = self.view_model.data_manager.get_data('platforms', {}).keys()
+            self.platform_dropdown.addItems(platforms)
+
+            # Restore the previous selection if it still exists in the new items
+            if current_text in platforms:
+                self.platform_dropdown.setCurrentText(current_text)
+        finally:
+            self._updating = False
+            print("Dropdown updated")
+
+    def on_platform_changed(self, text):
+        if self._updating:  # Prevent recursive updates
+            return
+
+        try:
+            self._updating = True
+            self.view_model.data_manager.set_data('last_platform', text)
+        finally:
+            self._updating = False
+            print(f"Platform changed to: {text}")
